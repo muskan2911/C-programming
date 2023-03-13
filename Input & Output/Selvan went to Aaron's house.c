b2eff55280270b99cea258e6bbc7b792677c88be
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2023-03-13 23:02:03 +0530

    Selvan went to Aaron's house.c

---
+++ Input & Output/Selvan went to Aaron's house.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{float num1,num2;
+double resnum1,resnum2;
+scanf("%f%f",&num1,&num2);
+resnum1=num1;
+
+    resnum2=num2;
+
+    printf("%lf\n%lf",resnum1,resnum2);
+
+	return 0;
+}
