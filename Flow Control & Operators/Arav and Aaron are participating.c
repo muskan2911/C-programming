b9449d8ac5d8bf8f2df79fc38e5133c14105fffa
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2023-03-14 23:04:32 +0530

    Arav and Aaron are participating.c

---
+++ Flow Control & Operators/Arav and Aaron are participating.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{ int aravspeed,aaronspeed,speeddiff;
+scanf("%d%d",&aravspeed,&aaronspeed);
+if(aravspeed>aaronspeed){
+    speeddiff=aravspeed-aaronspeed;
+    printf("%d",speeddiff);}
+else{
+    speeddiff=aaronspeed-aravspeed;
+    printf("%d",speeddiff);
+}
+	return 0;
+}
