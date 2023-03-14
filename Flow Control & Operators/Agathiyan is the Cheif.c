f734111b3852211aa3e1c35d03f3466949616015
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2023-03-14 23:03:35 +0530

    Agathiyan is the Cheif.c

---
+++ Flow Control & Operators/Agathiyan is the Cheif.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{int N;
+scanf("%d",&N);
+if(N<10)
+printf("Insufficient Earning");
+else if (N<100)
+printf("Very Low Earning");
+else if (N<1000)
+printf("Low Earning");
+else if (N<10000)
+printf("Sufficient Earning");
+else
+    printf("High Earning");
+	return 0;
+}
