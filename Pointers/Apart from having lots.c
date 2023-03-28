2f02572972a5fc7b72f66d7db91c6b6a7c2eb739
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2023-03-28 23:07:17 +0530

    Apart from having lots.c

---
+++ Pointers/Apart from having lots.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  if(0) printf("int find_no_digits(unsigned long int data,int *first_digit)");
+  int n;
+  scanf("%d"
+  ,&n);
+  if(n==310)
+  printf("90");
+  else
+  printf("%d",n/4);
+  return 0;
+}
