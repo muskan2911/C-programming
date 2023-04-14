27c58b51e1489c6b4dbb6a53dd67a5d0c8e101d6
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2023-04-15 01:14:06 +0530

    Ramesh have been given.c

---
+++ Structure Pointers and Array Pointers/Ramesh have been given.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+ void count(int a[],int n, int k){
+int *f,*temp,i;
+temp=(int*)malloc(n*sizeof(int));
+f=(int*)calloc(k,sizeof(int));
+for(i=0;i<n;i++)
+f[a[i]%k]++;
+for(i=k-2;i>=0;i--)
+f[i]=f[i]+f[i+1];
+for(i=n-1;i>=0;i--){
+temp[f[a[i]%k]-1]=a[i];
+f[a[i]%k]--;}
+for(i=0;i<n;i++)
+printf("%d ",temp[i]);
+ }
+ void sort(int a[],int n,int k,int m){
+int *temp,*f,i;
+f=(int*)calloc(m+1,sizeof(int));
+temp=(int*)malloc(n*sizeof(int));
+for(i=0;i<n;i++)
+f[a[i]]++;
+for(i=1;i<=m;i++)
+f[i]=f[i]+f[i-1];
+for(i=n-1;i>=0;i--){
+temp[f[a[i]]-1]=a[i];
+f[a[i]]--;
+}
+count(temp,n,k);
+ }
+ int main()
+ {
+ int n,k,i,*a,max=0;
+ scanf("%d %d",&n,&k);
+ a=(int*)malloc(n*sizeof(int));
+ for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+if(max<a[i])
+max=a[i];
+ }
+ sort(a,n,k,max);
+ return 0;}
